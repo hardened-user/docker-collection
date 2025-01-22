a52bfc2a4422c2f54dc176f1abcc3e8a034e303c
Aleksey K <aleksey.adm@gmail.com>
2025-01-22 15:11:09 +0300

    typofix

--- toolkit/docker-entrypoint.sh
+++ toolkit/docker-entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-export LC_ALL="C.UTF-8"
-export LANG="C.UTF-8"
-set -u
-#------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
 if [ -z "$@" ]; then
-    # starting cmd
-   exec /bin/bash -c "sleep infinity";
+  # starting cmd
+  exec /bin/bash -c "sleep infinity";
 else
-    # cutsom cmd (example: /bin/bash)
-    exec $@;
+  # overriding cmd (example: /bin/bash)
+  exec $@;
 fi
