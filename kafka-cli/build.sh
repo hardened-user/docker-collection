76deea66fb556ed8172d42a63756446126f84b7a
Aleksey K <aleksey.adm@gmail.com>
2025-11-24 14:03:28 +0300

    fix docker-entrypoint.sh

--- kafka-cli/build.sh
+++ kafka-cli/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 ###############################################################################
-docker build --pull --force-rm --progress=plain \
+docker build --pull --force-rm --network=host --progress=plain \
     --build-arg JAVA_VERSION=11 \
     --build-arg KAFKA_VERSION=3.5.2 \
     --build-arg SCALA_VERSION=2.13 \
