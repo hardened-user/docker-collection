b7b8514b2797957da8f77df849faacfd99ed2689
Aleksey K <aleksey.adm@gmail.com>
2026-04-02 20:31:55 +0300

    + microsocks:latest

--- microsocks/build.sh
+++ microsocks/build.sh
@@ -5,4 +5,6 @@ export MICROSOCKS_RELEASE="1.0.5"
 
 docker build --pull --force-rm --progress=plain \
     --build-arg "MICROSOCKS_RELEASE=${MICROSOCKS_RELEASE}" \
-    -t "microsocks:${MICROSOCKS_RELEASE}" -t "hardeneduser/microsocks:${MICROSOCKS_RELEASE}" .
+    -t "microsocks:${MICROSOCKS_RELEASE}" \
+    -t "hardeneduser/microsocks:${MICROSOCKS_RELEASE}" \
+    -t "hardeneduser/microsocks:latest" .
