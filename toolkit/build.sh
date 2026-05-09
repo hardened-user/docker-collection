66a91f2fefe31f63b340e1c05f522ceb536bcb52
Aleksey K <aleksey.adm@gmail.com>
2026-05-09 22:30:03 +0300

    feat

--- toolkit/build.sh
+++ toolkit/build.sh
@@ -2,4 +2,4 @@
 set -eu
 ###############################################################################
 docker build --pull --force-rm --progress=plain \
-   -t "hardeneduser/toolkit:latest" .
+   -t "hardeneduser/toolkit:latest" . $@
