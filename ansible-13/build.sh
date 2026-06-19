6076ed412723515894fe3f2c0f821a8bf38c118f
Aleksey K. <aleksey.adm@gmail.com>
2026-06-19 13:21:20 +0300

    update build.sh

--- ansible-13/build.sh
+++ ansible-13/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -eu
-###############################################################################
+# ----------------------------------------------------------------------------------------------------------------------
+# shellcheck disable=SC2068
 docker build --pull --force-rm --progress=plain \
-    --build-arg USER_UID=$(id -u) \
-    --build-arg USER_GID=$(id -g) \
-    -t "ansible:13" -t "hardeneduser/ansible:13" . $@
+    --build-arg USER_UID="$(id -u)" \
+    --build-arg USER_GID="$(id -g)" \
+    -t "hardeneduser/ansible:13" . $@
