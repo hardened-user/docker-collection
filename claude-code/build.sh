fc502f17b67a63efaf99a5663d1d1b0847746e39
Aleksey K. <aleksey.adm@gmail.com>
2026-08-05 17:59:00 +0300

    add --network=host

--- claude-code/build.sh
+++ claude-code/build.sh
@@ -3,7 +3,7 @@ set -eu
 # ----------------------------------------------------------------------------------------------------------------------
 NODE_VERSION="24"
 # shellcheck disable=SC2068
-docker build --pull --force-rm --progress=plain \
+docker build --pull --force-rm --network=host --progress=plain \
     --build-arg NODE_VERSION="${NODE_VERSION}" \
     --build-arg USER_UID="$(id -u)" \
     --build-arg USER_GID="$(id -g)" \
