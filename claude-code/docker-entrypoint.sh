f46ef1f42936165762f5211cc2fcd9dc4ffc904e
Aleksey K. <aleksey.adm@gmail.com>
2026-08-09 20:33:39 +0300

    bugfix

--- claude-code/docker-entrypoint.sh
+++ claude-code/docker-entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-set -eu
+set -euo pipefail
 # ----------------------------------------------------------------------------------------------------------------------
-chown "${USER_NAME}:${USER_NAME}" "${CLAUDE_CONFIG_DIR}"
-chmod 700 "${CLAUDE_CONFIG_DIR}"
-#
 if [[ "${1:-}" =~ ^(/.*/)?(ba)?sh ]]; then
-  # overriding cmd (example: /bin/bash)
+  # override cmd (example: docker run image /bin/bash)
   exec su-exec "${USER_NAME}" "$@"
 else
-  # starting cmd
-  exec su-exec "${USER_NAME}" /usr/bin/claude
+  # run default cmd
+  chown "${USER_NAME}:${USER_NAME}" "${CLAUDE_CONFIG_DIR}"
+  chmod 700 "${CLAUDE_CONFIG_DIR}"
+  exec su-exec "${USER_NAME}" /usr/bin/claude "$@"
 fi
